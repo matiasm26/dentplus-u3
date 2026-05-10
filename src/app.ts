968e1c598b3538540c79623fcfea8291e84f8335
Matías Alexander Martínez Velásquez <mmartinez@arrimaq.com>
2026-05-10 12:14:06 -0400

    feat: setup express typescript and handlebars

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { engine } from "express-handlebars";
+
+const app = express();
+const PORT = 3000;
+
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "DentPlus"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
